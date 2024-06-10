75ea99c4d6c13102c8c8a746b1619a48cf7ab3be
daksh3003 <dakshpandey3003@gmail.com>
2024-06-10 13:03:28 +0530

    initial outline

---
+++ src/components/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+
+    <div>
+        <h3>Shoes: 500rs</h3>
+        <button className=" btn btn-primary mx-2">-</button>
+        Add To Cart
+        <button className=" btn btn-primary mx-2">+</button>
+        </div>
+  )
+}
+
+export default Shop
